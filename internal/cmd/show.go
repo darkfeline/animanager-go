e1be1fb510628a3f205d7f4ed0579b241ea814aa
Allen Li <darkfeline@felesatra.moe>
2018-08-02 00:33:05 -0700

    Work

--- internal/cmd/show.go
+++ internal/cmd/show.go
@@ -19,14 +19,20 @@
 package cmd
 
 import (
+	"bufio"
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"github.com/google/subcommands"
+	"github.com/pkg/errors"
 	"go.felesatra.moe/animanager/internal/config"
 	"go.felesatra.moe/animanager/internal/database"
+	"go.felesatra.moe/animanager/internal/models"
 )
 
 type Show struct {
@@ -44,11 +50,47 @@ func (s *Show) SetFlags(f *flag.FlagSet) {
 }
 
 func (s *Show) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	if f.NArg() != 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s", s.Usage())
+		return subcommands.ExitUsageError
+	}
+	aid, err := strconv.Atoi(f.Arg(0))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: invalid AID: %s\n", err)
+		return subcommands.ExitUsageError
+	}
 	c := config.New()
-	_, err := database.Open(c.DBPath())
+	db, err := database.Open(c.DBPath())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error opening database: %s\n", err)
 		return subcommands.ExitFailure
 	}
+	if err := printAnime(os.Stdout, db, aid); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+		return subcommands.ExitFailure
+	}
 	return subcommands.ExitSuccess
+}
+
+func printAnime(w io.Writer, db *sql.DB, aid int) error {
+	r, err := db.Query(`select aid, title, type, episodecount, startdate, enddate from anime where aid=?`, aid)
+	if err != nil {
+		return errors.Wrap(err, "failed to query anime")
+	}
+	defer r.Close()
+	if !r.Next() {
+		return r.Err()
+	}
+	a := models.Anime{}
+	if err := r.Scan(&a.AID, &a.Title, &a.Type, &a.EpisodeCount, &a.StartDate, &a.EndDate); err != nil {
+		return errors.Wrap(err, "failed to scan anime")
+	}
+	bw := bufio.NewWriter(w)
+	fmt.Fprintf(bw, "AID: %d\n", a.AID)
+	fmt.Fprintf(bw, "Title: %s\n", a.Title)
+	fmt.Fprintf(bw, "Type: %s\n", a.Type)
+	fmt.Fprintf(bw, "Episodes: %d\n", a.EpisodeCount)
+	fmt.Fprintf(bw, "Start date: %s\n", a.StartDate)
+	fmt.Fprintf(bw, "End date: %s\n", a.EndDate)
+	return bw.Flush()
 }
