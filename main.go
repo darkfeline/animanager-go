232e79da8524df170dfa98fabdab0b0666cd5293
Allen Li <darkfeline@felesatra.moe>
2018-08-10 16:45:59 -0700

    Work

--- main.go
+++ main.go
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
+// Command animanager manages watched anime and anime to be watched.
 package main
 
 import (
