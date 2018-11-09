5a4ebe0159747183fa5ba156e93d99b80f234d78
Allen Li <darkfeline@felesatra.moe>
2018-11-09 01:02:14 -0800

    Add PrintError

--- internal/cmd/log.go
+++ internal/cmd/log.go
@@ -1,9 +1,28 @@
 package cmd
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+
+	"go.felesatra.moe/go2/errors"
 )
 
 // Logger is used by this package for logging.
 var Logger = log.New(ioutil.Discard, "cmd: ", log.LstdFlags)
+
+type userError interface {
+	error
+	UserError() string
+}
+
+// PrintError is used by this package for printing user facing errors.
+var PrintError func(error) = func(err error) {
+	var err2 userError
+	if errors.AsValue(err2, err) {
+		fmt.Fprintln(os.Stderr, err2.UserError())
+	} else {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
