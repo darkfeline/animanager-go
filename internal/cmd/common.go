02a7cec0b716de1763200d6c9be6ddc5b9577786
Allen Li <darkfeline@felesatra.moe>
2018-11-14 01:00:35 -0800

    Fix bug

--- internal/cmd/common.go
+++ internal/cmd/common.go
@@ -38,7 +38,7 @@ type userError interface {
 // PrintError is used by this package for printing user facing errors.
 var PrintError func(error) = func(err error) {
 	var err2 userError
-	if errors.AsValue(err2, err) {
+	if errors.AsValue(&err2, err) {
 		fmt.Fprintln(os.Stderr, err2.UserError())
 	} else {
 		fmt.Fprintln(os.Stderr, errors.Format(err, false))
