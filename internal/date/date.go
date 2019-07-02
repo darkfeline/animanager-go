14373a90991a64878184afd938ab5f9999b791e8
Allen Li <darkfeline@felesatra.moe>
2019-07-01 22:23:24 -0700

    Improve doc

--- internal/date/date.go
+++ internal/date/date.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
-// Package date implements a date type.
+// Package date implements a date type stored as Unix timestamps.
 package date
 
 import "time"
