3674e024d0d0d11566df310f554869de4402c934
Allen Li <darkfeline@felesatra.moe>
2021-01-02 01:28:41 -0800

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Allen Li'
 author = 'Allen Li'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.17.0'
+release = 'v0.18.0'
 
 
 # -- General configuration ---------------------------------------------------
