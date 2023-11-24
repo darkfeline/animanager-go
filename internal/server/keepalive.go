c3002fd8ca8d1ee7d9713678a6c6c402554bd719
Allen Li <darkfeline@felesatra.moe>
2023-11-24 03:10:10 -0800

    Add keepalive func

---
+++ internal/server/keepalive.go
@@ -0,0 +1,56 @@
+// Copyright (C) 2023  Allen Li
+//
+// This file is part of Animanager.
+//
+// Animanager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Animanager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
+
+package server
+
+import (
+	"context"
+	"errors"
+	"time"
+)
+
+type pingFunc func(context.Context) (port string, _ error)
+
+// startKeepalive starts a goroutine that calls the ping function at intervals.
+// The returned function must be called to stop the goroutine.
+func startKeepalive(p pingFunc, interval time.Duration, l Logger) (stop func()) {
+	c := make(chan struct{})
+	stop = func() { close(c) }
+	ctx := context.Background()
+	ctx, cancel := context.WithCancelCause(ctx)
+	go func() {
+		<-c
+		cancel(errors.New("keepalive stopped"))
+	}()
+	go func() {
+		t := time.NewTicker(interval)
+		for {
+			select {
+			case <-t.C:
+				ctx, cancel := context.WithTimeoutCause(ctx, 2*time.Second, errors.New("keepalive ping timeout"))
+				if _, err := p(ctx); err != nil {
+					l.Printf("keepalive ping: %s", err)
+				}
+				cancel()
+			case <-c:
+				t.Stop()
+				return
+			}
+		}
+	}()
+	return stop
+}
