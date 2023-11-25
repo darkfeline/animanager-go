29e2f15929fb4d4cd97db46eeeb01c8f67bc094b
Allen Li <darkfeline@felesatra.moe>
2023-11-24 22:57:56 -0800

    Implement matchFileToEpisodes

--- internal/fileid/udp.go
+++ internal/fileid/udp.go
@@ -22,6 +22,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strconv"
 
 	"go.felesatra.moe/anidb/udpapi"
 	"go.felesatra.moe/animanager/internal/udp"
@@ -30,6 +31,10 @@ import (
 
 var fmask udpapi.FileFmask
 var amask udpapi.FileAmask
+
+func init() {
+	fmask.Set("aid", "eid")
+}
 
 // MatchEpisode adds the given file as an episode file.
 // Episode matching is done via AniDB UDP API.
@@ -51,4 +56,43 @@ func MatchEpisode(ctx context.Context, db *sql.DB, c *udp.Client, file string) e
 	}
 	_ = rows
 	panic(nil)
+}
+
+// matchFileToEpisodes finds episode matches for the given file.
+// Episode matching is done via AniDB UDP API.
+func matchFileToEpisodes(ctx context.Context, c *udp.Client, file string) (epMatch, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return epMatch{}, fmt.Errorf("match file to episode: %s", err)
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		return epMatch{}, fmt.Errorf("match file to episode: %s", err)
+	}
+	h := ed2k.New()
+	sum := h.Sum(nil)
+	row, err := c.FileByHash(ctx, fi.Size(), fmt.Sprintf("%x", sum), fmask, amask)
+	if err != nil {
+		return epMatch{}, fmt.Errorf("match file to episode: %s", err)
+	}
+	if n := len(row); n != 3 {
+		return epMatch{}, fmt.Errorf("match file to episode: unexpected number of values in response: %d", n)
+	}
+	aid, err := strconv.Atoi(row[1])
+	if err != nil {
+		return epMatch{}, fmt.Errorf("match file to episode: parse aid: %s", err)
+	}
+	eid, err := strconv.Atoi(row[2])
+	if err != nil {
+		return epMatch{}, fmt.Errorf("match file to episode: parse eid: %s", err)
+	}
+	return epMatch{
+		aid: aid,
+		eid: eid,
+	}, nil
+}
+
+type epMatch struct {
+	aid int
+	eid int
 }
