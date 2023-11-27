d31649407346c159f1af31b53606c0282b77a0e4
Allen Li <darkfeline@felesatra.moe>
2023-11-26 16:31:07 -0800

    Drop episodekey unique constraint

--- internal/database/migrate/migrate.go
+++ internal/database/migrate/migrate.go
@@ -49,6 +49,7 @@ var migrationSet = migrate.NewMigrationSet([]migrate.Migration{
 	{From: 6, To: 7, Func: migrate7},
 	{From: 7, To: 8, Func: migrate8},
 	{From: 8, To: 9, Func: migrate9},
+	{From: 9, To: 10, Func: migrate10},
 })
 
 func getUserVersion(d *sql.DB) (int, error) {
---
+++ internal/database/migrate/migrate10.go
@@ -0,0 +1,80 @@
+// Copyright (C) 2021  Allen Li
+//
+// This file is part of Animanager.
+//
+// Animanager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Animanager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
+
+package migrate
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+func migrate10(ctx context.Context, d *sql.DB) error {
+	c, err := d.Conn(ctx)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	_, err = c.ExecContext(ctx, "PRAGMA foreign_keys = 0")
+	if err != nil {
+		return err
+	}
+	defer c.ExecContext(ctx, "PRAGMA foreign_keys = 1")
+
+	t, err := c.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer t.Rollback()
+
+	_, err = t.Exec(`
+CREATE TABLE episode_new (
+    eid INTEGER,
+    aid INTEGER NOT NULL,
+    type INTEGER NOT NULL,
+    number INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    length INTEGER NOT NULL,
+    user_watched INTEGER NOT NULL CHECK (user_watched IN (0, 1))
+        DEFAULT 0,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (aid) REFERENCES anime (aid)
+        ON DELETE CASCADE ON UPDATE CASCADE
+)`)
+	if err != nil {
+		return fmt.Errorf("CREATE TABLE episode_new: %s", err)
+	}
+	_, err = t.Exec(`
+INSERT INTO episode_new
+(eid, aid, type, number, title, length, user_watched)
+SELECT eid, aid, type, number, title, length, user_watched
+FROM episode`)
+	if err != nil {
+		return fmt.Errorf("INSERT INTO episode_new: %s", err)
+	}
+	_, err = t.Exec(`DROP TABLE episode`)
+	if err != nil {
+		return fmt.Errorf("DROP TABLE episode: %s", err)
+	}
+	_, err = t.Exec(`ALTER TABLE episode_new RENAME TO episode`)
+	if err != nil {
+		return fmt.Errorf("ALTER TABLE episode_new: %s", err)
+	}
+
+	return t.Commit()
+}
