777bfc807d58612c10ca2144f2ad871af6339681
Allen Li <darkfeline@felesatra.moe>
2024-01-14 21:08:45 -0800

    Add doc

--- internal/sqlc/gen.go
+++ internal/sqlc/gen.go
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Animanager.  If not, see <http://www.gnu.org/licenses/>.
 
+// Package sqlc contains type-safe wrappers for SQL queries generated
+// using sqlc.
 package sqlc
 
 //go:generate sqlc generate
